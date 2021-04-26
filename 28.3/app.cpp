b0e0d182c9d187a1fa8f6e38ee2ad2ecd8352184
Jaroshevskii <jaroshevskiisasha@gmail.com>
2021-04-26 12:44:53 +0300

    Update README.md

---
+++ 28.3/app.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+
+void fileWrite(const char* file) {
+  std::ofstream fout(file);
+  
+  // Якщо не можливо відкрити файл для запису
+  if (!fout) {
+    std::cout << "ERROR: Could not be opened for writing " << file << '\n';
+    return;
+  }
+  
+  std::cout << "// Use 'end' to end writing to file\n";
+  std::cout << '\n';
+  
+  while (true) {
+    std::cout << "> ";
+    std::string str;
+    std::getline(std::cin, str);
+    
+    // Якщо введена команда 'end' закінчення запису
+    if (str == "end") {
+      return;
+    }
+    
+    // Запис рядка в файл
+    fout << str << '\n';
+  }
+}
+
+void fileRead(const char* file) {
+  std::ifstream fin(file);
+  
+  // Якщо не можливо відкрити файл для читання
+  if (!fin) {
+    std::cout << "ERROR: Could not be opened for reading " << file << '\n';
+    return;
+  }
+  
+  while (fin) {
+    std::string str;
+    std::getline(fin, str);
+    
+    // Вивід рядка
+    std::cout << str << '\n';
+  }
+}
+
+int main() {
+  const char file[] = "28.3/temp.txt";
+  
+  // Запис в файл
+  fileWrite(file);
+  
+  return 0;
+}
