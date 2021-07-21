fca92940dedf900dbe3c8d3b011a14e20bcaf005
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-07-21 19:56:07 +0300

    Add 28.2

---
+++ 28/28.2/app.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+
+void printGreeting(const char* fileName) {
+  std::ofstream fout(fileName);
+
+  // Не можливо відкрити файл для запису
+  if (!fout) {
+    std::cout << "ERROR: Could not be opened for writing '" << fileName << "'.\n";
+    std::cout << '\n';
+    return;
+  }
+
+  // Робота вчителя ой як нелегка.
+  // Але ви всі ці дні витрачаєте сили, 
+  // Щоб з нас зробити розумнішими та працьовитішими.
+  // Щоб не соромно Вам було за нас!
+  // Ми щиро Вам раді за старання.
+  // І ось сьогодні ми вітаємо Вас з днем народження.
+  // Ми хочемо Вам побажати здоров'я, любові і щастя.
+  fout << "The work of a teacher is not easy.\n"
+          "But you are wasting all these days,\n"
+          "To make us smarter and harder working.\n"
+          "So that you are not ashamed of us!\n"
+          "We sincerely welcome you for your efforts.\n"
+          "And today we congratulate you on your birthday.\n"
+          "We wish you health, love and happiness.\n";
+}
+
+int main() {
+  printGreeting("greeting.txt");
+  return 0;
+}   
