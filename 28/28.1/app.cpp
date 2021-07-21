7512ff6f27d3ed0601ee1ef9f46638ce625708fc
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-07-21 16:21:17 +0300

    Add 28.1

---
+++ 28/28.1/app.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void writeFile(const char* fileName) {
+  std::ofstream fout(fileName);
+  
+  // Неможливо відкрити файл для запису
+  if (!fout) {
+    std::cout << "ERROR: Could not be opened for writing '" << fileName << "'.\n";
+    std::cout << '\n';
+    return;
+  }
+
+  std::string str;
+  for (auto i = 0; i != 2; ++i) {
+    std::cout << "> String = ";
+    getline(std::cin, str);
+    std::cout << '\n';
+
+    fout << str << '\n';
+  }
+}
+
+void readFile(const char* fileName) {
+  std::ifstream fin(fileName);
+
+  // Неможливо відкрити файл для читання
+  if (!fin) {
+    std::cout << "ERROR: Could not be opened for reading '" << fileName << "'.\n";
+    std::cout << '\n';
+    return;
+  }
+  
+  std::string str;
+  while (getline(fin, str)) {
+    std::cout << str << '\n';
+  }
+  std::cout << '\n';
+}
+int main() {
+  writeFile("temp.txt");
+
+  readFile("temp.txt");
+  return 0;
+}
