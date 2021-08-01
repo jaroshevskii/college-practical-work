f0002978430a770b38f9692875246f1d1a577f3f
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-08-01 23:46:59 +0300

    Add 5.1

---
+++ 5/5.1/app.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+double setResult(const int16_t &a, const int16_t &b, const int16_t &c,
+                 const double &x) {
+  if (x < 1.0 && c != 0) {
+    return a * pow(x, 2) + b / c;
+  } else if (x > 1.5 && c == 0) {
+    return x - a / pow(x - c, 2);
+  } else if (c != 0) {
+    return pow(x, 2) / pow(c, 2);
+  }
+  
+  std::cout << "ERROR: Division by zero.\n";
+  std::cout << '\n';
+  return 0.0;
+}
+
+void printResult(const double &result) {
+  std::cout << "Result: " << result << '\n';
+  std::cout << '\n';
+}
+
+int main() {
+  std::cout << "// Enter the real numbers a, d and c.\n";
+  std::cout << '\n';
+
+  std::cout << "> a = ";
+  int16_t a;
+  std::cin >> a;
+
+  std::cout << "> b = ";
+  int16_t b;
+  std::cin >> b;
+
+  std::cout << "> c = ";
+  int16_t c;
+  std::cin >> c;
+  std::cout << '\n';
+
+  std::cout << "// Enter a fractional number x.\n";
+  std::cout << '\n';
+  
+  std::cout << "> x = ";
+  double x;
+  std::cin >> x;
+  std::cout <<  '\n';
+
+  double result = setResult(a, b, c, x);
+
+  printResult(result);
+  return 0;
+}
