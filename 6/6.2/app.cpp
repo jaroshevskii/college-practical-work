bd8709833e97bc1a312cd717260928936274e3aa
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-08-02 19:40:24 +0300

    Add 6.2

--- 6/6.2/app.cpp
+++ 6/6.2/app.cpp
@@ -5,7 +5,7 @@ uint16_t getFunctionNumber() {
   while (true) {
     std::cin >> functionNumber;
 
-    if (functionNumber >= 1 && functionNumber <= 7) {
+    if (functionNumber >= 1 && functionNumber <= 3) {
       return functionNumber;
     } else {
       std::cout << '\n';
@@ -17,12 +17,39 @@ uint16_t getFunctionNumber() {
   }
 }
 
+double setResult(const uint16_t &functionNumber, const double &x) {
+  switch (functionNumber) {
+  case 1:
+    return -2 * pow(x, 2) - 4;
+  case 2:
+    return 5 * x + 2;
+  case 3:
+    return 15 - 3 * x;
+  default:
+    return 0.0;
+  }
+}
+
+void printResult(const double &result) {
+  std::cout << "Result: " << result << '\n';
+  std::cout << '\n';
+}
 
 int main() {
+  std::cout << "// Enter a fractional number x.\n";
+               "> x = ";
+  double x;
+  std::cin >> x;
+  std::cout << '\n';
+
   std::cout << "// Enter the function number.\n"
                "> ";
   uint16_t functionNumber = getFunctionNumber();
   std::cout << '\n';
-
+  
+  // Задати результат обчислення
+  double result = setResult(functionNumber, x);
+  
+  printResult(result);
   return 0;
 }
