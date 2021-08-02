9c2bcac4aab4d94bd18f3b2ac700dc017b61d305
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-08-03 01:25:49 +0300

    Update 7.1

--- 7/7.1/app.cpp
+++ 7/7.1/app.cpp
@@ -2,11 +2,11 @@
 
 int main() {
   for (uint16_t i = 0; i != 10; ++i) {
-    std::cout << "--------------------------------\n"
-                 "First name: Sania\n"
+    std::cout << std::string(32, '-') << '\n'
+              << "First name: Sania\n"
                  "Last name: Jaroshevskii\n";
   }
-  std::cout << "--------------------------------\n";
+  std::cout << std::strin(32, '-') << '\n';
   std::cout << '\n';
   return 0;
 }
