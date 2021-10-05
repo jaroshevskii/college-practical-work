7e8013f70fb49771520fd1e63f777b2bc8579c33
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-10-05 20:01:57 +0300

    Add Get.cpp

---
+++ 5/5.1/Get.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/// Отримати ціле число
+int getInt() {
+  int number;
+  std::cin >> number;
+  return number;
+}
+
+/// Отримати десяткове число
+double getDouble() {
+  double number;
+  std::cin >> number;
+  return number;
+}
