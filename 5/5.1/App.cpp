53e5ed8cf4a16578b861822ed0499f50154dcd3e
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-10-05 20:07:55 +0300

    Update App.cpp

---
+++ 5/5.1/App.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include <Get.h>
+
+/// Задати результат
+double setResult(const int &a, const int &b, const int &c,
+                 const double &x) {
+  if (x < 1.0 && c != 0)
+    return a * pow(x, 2) + b / c;
+  if (x > 1.5 && c == 0)
+    return x - a / pow(x - c, 2);
+  if (c != 0)
+    return pow(x, 2) / pow(c, 2);
+  }
+
+  std::cout << "ERROR: Division by zero.\n";
+  std::cout << '\n';
+  return 0.0;
+}
+
+/// Вивести результат
+void printResult(const double &result) {
+  std::cout << "Result: " << result << '\n';
+  std::cout << '\n';
+}
+
+int main() {
+  std::cout << "// Enter the integers: a, b, and c.\n"
+               "> a = ";
+  int a = getInt();
+
+  std::cout << "> b = ";
+  int b = getInt();
+
+  std::cout << "> c = ";
+  int c = getInt();
+  std::cout << '\n';
+
+  std::cout << "// Enter the decimal number.\n"
+               "> x = ";
+  double x = getDouble();
+  std::cout << '\n';
+
+  double result = setResult(a, b, c, x);
+
+  printResult(result);
+  return 0;
+}
