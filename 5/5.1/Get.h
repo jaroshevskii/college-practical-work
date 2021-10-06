f8f0c6975500ced4d9f2ebdebe93ddb2bf7a761d
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-10-06 21:19:19 +0300

    Update Get.h

--- 5/5.1/Get.h
+++ 5/5.1/Get.h
@@ -1,10 +1,18 @@
 #ifndef GET_H
 #define GET_H
 
-/// Отримати ціле число
-int getInt();
+#include <iostream>
 
-/// Отримати десяткове число
-double getDouble();
+/// Отримати ціле число.
+int getInt(int &number) {
+  std::cin >> number;
+  return number;
+}
+
+/// Отримати десяткове число.
+double getDouble(double &number) {
+  std::cin >> number;
+  return number;
+}
 
 #endif
