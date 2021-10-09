cdcb21357d78e04862318989452c03d52ed7ee87
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-10-10 00:16:03 +0300

    Update Get.h

---
+++ 6/6.2/Get.h
@@ -0,0 +1,18 @@
+#ifndef GET_H
+#define GET_H
+
+#include <iostream>
+
+/// Отримати ціле число.
+int getInt(int &number) {
+  std::cin >> number;
+  return number;
+}
+
+/// Отримати десяткове число.
+double getDouble(double &number) {
+  std::cin >> number;
+  return number;
+}
+
+#endif // GET_H
