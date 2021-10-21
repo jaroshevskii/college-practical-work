09c2857ba8d9cc6e4ae7c6df8272696b00e69291
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-10-22 00:28:24 +0300

    Update Task7.2.cpp

--- PracticalWork7/Task7.2/Task7.2.cpp
+++ PracticalWork7/Task7.2/Task7.2.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 
+#include "Constants.h"
+
 /// Встановити результат.
-double setResult(const double &pi, const int &x) {
-  return 3 * cos(2) * (x * pi - pi / 4);
+double setResult(const int &x) {
+  return 3 * cos(2) * (x * Constants::pi - Constants::pi / 4);
+}
+
+/// Надрукувати результат.
+void printResult() {
+  double result;
+
+  for (int x = -3; x <= 3; ++x) {
+    result = setResult(x);
+    
+    std::cout << std::string(32, '-') << '\n'
+              << "Result: " << result << '\n';
+  }
+
+  std::cout << std::string(32, '-') << '\n';
+  std::cout << '\n';
 }
 
 int main() {
-  const double pi = 3.14159265359;
-
-  double result;
-  for (int x = -3; x <= 3; ++x) {
-    std::cout << std::string(32, '-') << '\n';
-
-    result = setResult(pi, x);
-
-    std::cout << "Result: " << result << '\n';
-  }
-  std::cout << std::string(32, '-') << '\n';
-  std::cout << '\n';
+  printPresult();
   return 0;
 }
