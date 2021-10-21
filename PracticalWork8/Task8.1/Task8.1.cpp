5829c58bd663778efed0be8e1a38eaf9cb3f7623
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-10-22 00:29:20 +0300

    Update Task8.1.cpp

--- PracticalWork8/Task8.1/Task8.1.cpp
+++ PracticalWork8/Task8.1/Task8.1.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-  const char symbol = 'j'; // Символ.
-  const int nRow = 4;      // Кількість рядків.
-  const int nColumn = 9;   // Кількість стовбчиків.
+  const char symbol = 'j'; // Символ
+  const int nRow = 4;      // Кількість рядків
+  const int nColumn = 9;   // Кількість стовбчиків
 
   for (int i = 0; i <= nRow; ++i) {
     for (int j = 0; j <= nColumn; ++j) {
